4d5fa7399982430c65f4d3b18d47e8a20c844d16
eliseu gili <gili.eliseup@gmail.com>
2022-04-12 16:16:58 -0300

    replace test classes with JUnit

--- Classes de Teste/ControladorDeErrosTest.java
+++ Classes de Teste/ControladorDeErrosTest.java
@@ -1,3 +1,4 @@
+import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
@@ -16,6 +17,11 @@ public class ControladorDeErrosTest {
 	}
 
 	@Test
+	public void NumeroDeChancesMenorQueZero() throws Exception {
+		assertThrows(Exception.class, () -> controlador = new ControladorDeErros(-1));
+	}
+
+	@Test
 	public void registrandoUmErroDeUm() throws Exception {
 		controlador = new ControladorDeErros(1);
 
@@ -25,20 +31,34 @@ public class ControladorDeErrosTest {
 	}
 
 	@Test
-	public void copiando() throws Exception{
+	public void excedendoONumeroDeChances() throws Exception {
 		controlador = new ControladorDeErros(1);
-		
+
+		controlador.registreUmErro();
+
+		assertThrows(Exception.class, () -> controlador.registreUmErro());
+	}
+
+	@Test
+	public void construtorComParametroNegativo() {
+		assertThrows(Exception.class, () -> controlador = new ControladorDeErros(null));
+	}
+
+	@Test
+	public void copiando() throws Exception {
+		controlador = new ControladorDeErros(1);
+
 		ControladorDeErros c = new ControladorDeErros(controlador);
-		
+
 		assertEquals(controlador.equals(c), true);
 	}
 	
-//	@Test
-//	public void clonando() throws Exception{
-//		controlador = new ControladorDeErros(1);
-//		
-//		ControladorDeErros c = (ControladorDeErros) controlador.clone();
-//		
-//		assertEquals(controlador.equals(c), true);
-//	}
+	@Test
+	public void clonando() throws Exception{
+		controlador = new ControladorDeErros(1);
+		
+		ControladorDeErros c = (ControladorDeErros) controlador.clone();
+		
+		assertEquals(controlador.equals(c), true);
+	}
 }
--- Classes de Teste/PalavraTest.java
+++ Classes de Teste/PalavraTest.java
@@ -1,33 +1,39 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
 public class PalavraTest {
 
 	Palavra palavra = null;
-	
+
 	@Test
 	public void quantidade() throws Exception {
 		palavra = new Palavra("java");
 
 		int qtd = palavra.getQuantidade('a');
-		
-		assertEquals(2, qtd);		
+
+		assertEquals(2, qtd);
 	}
-	
+
+	@Test
+	public void construtorParametroNull() {
+		assertThrows(Exception.class, () -> palavra = new Palavra(null));
+	}
+
 	@Test
 	public void tamanho() throws Exception {
 		palavra = new Palavra("java");
-		
+
 		int tamanho = palavra.getTamanho();
-		
-		assertEquals(4, tamanho);		
+
+		assertEquals(4, tamanho);
 	}
-	
+
 	@Test
 	public void iezimaOcorrencia() throws Exception {
 		palavra = new Palavra("desenvolvimento");
-		
+
 		int posicao = palavra.getPosicaoDaIezimaOcorrencia(0, 'e');
 
 		assertEquals(posicao, 1);
